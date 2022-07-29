4ccf3c652b5eaaf8d3579fdd2183918fc1b53106
wtzhu13 <wtzhu_13@163.com>
2022-07-29 18:59:37 +0800

    modify sth

--- Demosaic/src/demosaic.m
+++ Demosaic/src/demosaic.m
@@ -7,15 +7,15 @@
 clc;clear;close all;
 
 %% ------------Raw Format----------------
-filePath = 'images/kodim19_8bits_RGGB.raw';
-bayerFormat = 'RGGB';
-width = 512;
-height= 768;
-bits = 8;
+filePath = 'images/rkisp_sc8238cs_D75_3840_2160_10bpp_1.2x_0.030s_normal_normL_single_183702980.raw';
+bayerFormat = 'BGGR';
+width = 3840;
+height= 2160;
+bits = 10;
 %% --------------------------------------
 bayerData = readRaw(filePath, bits, width, height);
 figure();
-imshow(bayerData);
+imshow(uint8(bayerData));
 title('raw image');
 
 %% expand image inorder to make it easy to calculate edge pixels
@@ -68,15 +68,45 @@ for ver = 2:height + 1
             case 'GBGR'
                 continue;
             case 'BGGR'
-                continue;
+                % G R -> B
+                if(0 == mod(ver, 2) && 0 == mod(hor, 2))
+                    imDst(ver, hor, 3) = bayerPadding(ver, hor);
+                    % G -> B
+                    imDst(ver, hor, 2) = (bayerPadding(ver-1, hor) + bayerPadding(ver+1, hor) +...
+                                         bayerPadding(ver, hor-1) + bayerPadding(ver, hor+1))/4;
+                    % R -> B
+                    imDst(ver, hor, 1) = (bayerPadding(ver-1, hor-1) + bayerPadding(ver-1, hor+1) + ...
+                                         bayerPadding(ver+1, hor-1) + bayerPadding(ver+1, hor+1))/4; 
+                % G B -> R
+                elseif (1 == mod(ver, 2) && 1 == mod(hor, 2))    
+                    imDst(ver, hor, 1) = bayerPadding(ver, hor);
+                    % G -> B
+                    imDst(ver, hor, 2) = (bayerPadding(ver-1, hor) + bayerPadding(ver+1, hor) +...
+                                         bayerPadding(ver, hor-1) + bayerPadding(ver, hor+1))/4;
+                    % R -> B
+                    imDst(ver, hor, 3) = (bayerPadding(ver-1, hor-1) + bayerPadding(ver-1, hor+1) + ...
+                                         bayerPadding(ver+1, hor-1) + bayerPadding(ver+1, hor+1))/4; 
+                elseif(0 == mod(ver, 2) && 1 == mod(hor, 2))
+                    imDst(ver, hor, 2) = bayerPadding(ver, hor);
+                    % R -> Gr
+                    imDst(ver, hor, 3) = (bayerPadding(ver, hor-1) + bayerPadding(ver, hor+1))/2;
+                    % B -> Gr
+                    imDst(ver, hor, 1) = (bayerPadding(ver-1, hor) + bayerPadding(ver+1, hor))/2;
+                elseif(1 == mod(ver, 2) && 0 == mod(hor, 2))
+                    imDst(ver, hor, 2) = bayerPadding(ver, hor);
+                    % B -> Gb
+                    imDst(ver, hor, 1) = (bayerPadding(ver, hor-1) + bayerPadding(ver, hor+1))/2;
+                    % R -> Gb
+                    imDst(ver, hor, 3) = (bayerPadding(ver-1, hor) + bayerPadding(ver+1, hor))/2;
+                end
         end
     end
 end
 imDst = uint8(imDst(2:height+1,2:width+1,:));
 figure,imshow(imDst);title('demosaic image');
 
-orgImage = imread('images/kodim19.png');
-figure, imshow(orgImage);title('org image');
+% orgImage = imread('images/kodim19.png');
+% figure, imshow(orgImage);title('org image');
 
 
 
